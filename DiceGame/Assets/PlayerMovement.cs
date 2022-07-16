7f8db874751ad56b60b600b2a8f3e34326a182b5
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2022-07-16 09:57:21 -0600

    player movement

---
+++ DiceGame/Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MovementSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var sideMovement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(sideMovement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        var upMovement = Input.GetAxis("Vertical");
+        Vector3 upMovementVector = new Vector3(0, upMovement, 0);
+        transform.Translate(upMovementVector * Time.deltaTime * MovementSpeed);
+    }
+
+    /*void FixedUpdate()
+    {
+        moveparams = (horizontalMove, 0f, 0f);
+        controller.Move(moveparams);
+    }*/
+}
